e6f0589afa8f20dba60093e63ea0ab83f147842a
Vitor Gabriel Pereira Trindade <gabrielptrindade02@gmail.com>
2026-05-08 09:18:22 -0300

    Estrutura Inicial do Backend

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Servidor Express rodando!' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
