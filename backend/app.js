76399e36f593feb3eba8cdd99c0b256fee233a65
Vitor Gabriel Pereira Trindade <gabrielptrindade02@gmail.com>
2026-05-12 15:12:22 -0300

    Adicione a documentação Swagger e integre o swagger-ui-express para a documentação da API.

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const loadDatabaseConfig = require('./src/config/database');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
 
 const app = express();
 
@@ -11,6 +14,12 @@ loadDatabaseConfig();
 
 const routes = require('./src/routes/index');
 app.use('/api', routes);
+
+const swaggerDocument = YAML.load(
+    path.join(__dirname, 'src', 'swagger.yaml')
+);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
